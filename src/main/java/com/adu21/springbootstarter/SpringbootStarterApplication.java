e3bcf2e380347185272cdb2c64ae7041d3d66747
ADU <LukeDu571@gmail.com>
2023-11-05 13:38:24 -0800

    Init

---
+++ src/main/java/com/adu21/springbootstarter/SpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.adu21.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStarterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/adu21/springbootstarter/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.adu21.springbootstarter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;≠
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, word!";
+    }
+}
